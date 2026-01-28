e0fd0516e00fa5770e870b20162a6bfe744a4dd7
Gabriel Martins de Oliveira <gabrieuz.martins@gmail.com>
2026-01-28 17:16:29 -0300

    Adicionando paginacao no endpoint do /livros

--- BACKENDLIVROSEBACPYTHON/main.py
+++ BACKENDLIVROSEBACPYTHON/main.py
@@ -13,10 +13,32 @@
 
 #CRUD - Create, Read, Update, Delete
 
-from fastapi import FastAPI, HTTPException
+# Documentação Swagger - Documentar os endpoints da nossa API (aplicação)
+
+# Acesse minha documentação swagger: http://127.0.0.1:8000/docs#/
+
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import Optional
-app = FastAPI()
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+import os
+
+
+app = FastAPI(
+    title="API de Livros",
+    description="API para gerenciar uma coleção de livros",
+    version="1.0.0",
+    contact={
+        "name":"Gabriel Martins",
+        "email":"gabrieuz.martins@gmail.com"
+    }
+)
+
+MEU_USUARIO = "admin" 
+MIHA_SENHA = "admin"
+
+security = HTTPBasic()
 
 meus_livrozinhos = {}
 
@@ -25,19 +47,55 @@ class Livro(BaseModel):
     autor_livro: str
     ano_livro: int
 
+def autenticar_usuario(credentials: HTTPBasicCredentials = Depends(security)):
+    is_username_correct = secrets.compare_digest(credentials.username, MEU_USUARIO)
+    is_password_correct = secrets.compare_digest(credentials.password, MIHA_SENHA)
+
+    if not (is_username_correct and is_password_correct):
+        raise HTTPException(
+            status_code=401,
+            detail="Usuário ou senha incorretos",
+            headers={"WWW-Authenticate": "Basic"}
+        )
+
+
 @app.get("/livros")
-def get_livros():
+def get_livros(page: int = 1, limit: int = 10,credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
+    if page < 1 or limit < 1:
+        raise HTTPException(status_code=400, detail="Page ou limit estão com valores inválidos")
     if not meus_livrozinhos:
         return {"message": "Nenhum livro cadastrado"}
-    else:
-        return {"Livros": meus_livrozinhos}
+    
+    start = (page-1) * limit
+    end = start + limit
+
+    livros_paginados = [
+        {"id": id_livro, "nome_livro": livro_data ["nome_livro"], "autor_livro": livro_data["autor_livro"], "ano_livro": livro_data["ano_livro"]}
+        for id_livro, livro_data in list(meus_livrozinhos.items())[start:end]
+        #for loop dentro dos meus_livrozinhos, onde eu estruturo as info dentro de uma lista (transformo em lista)
+        #Dando for loop para pegar o id_livro e livro_data usando a paginação estruturada pelo start e end
+        #Ao mesmo tempo eu crio um dicionário com as informações que eu quero retornar
+    ]
+    
+    return{
+        "page": page,
+        "limit": limit,
+        "total_livros": len(meus_livrozinhos),
+        "livros": livros_paginados
+    }
+# Essa função mostratrá os livros cadastrados na minha API de forma paginada como no retorno acima
+
+# http://127.0.0.1:8000/livros?page=1&limit=100
+# aqui eu escolho a página e a quantidade de livros que eu quero ver por página
+
+    
 # id do livro
 # nome do livro
 # autor do livro
 # ano de publicação
 
 @app.post("/adiciona")
-def post_livros(id_livro: int, livro: Livro):
+def post_livros(id_livro: int, livro: Livro, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
     if id_livro in meus_livrozinhos:
         raise HTTPException(status_code=400, detail="Livro já cadastrado")
     else:
@@ -46,7 +104,7 @@ def post_livros(id_livro: int, livro: Livro):
     
 
 @app.put("/atualiza/{id_livro}")
-def put_livros(id_livro: int, livro: Livro):
+def put_livros(id_livro: int, livro: Livro, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
     meu_livro = meus_livrozinhos.get(id_livro)
     if not meu_livro:
         raise HTTPException(status_code=404, detail="Livro não encontrado")
@@ -60,7 +118,7 @@ def put_livros(id_livro: int, livro: Livro):
         return {"message": "Livro atualizado com sucesso!"}
     
 @app.delete("/deletar/{id_livro}")
-def delet_livro(id_livro: int):
+def delet_livro(id_livro: int, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
     if id_livro not in meus_livrozinhos:
         raise HTTPException(status_code=404, detail="Livro não encontrado")
     else:
